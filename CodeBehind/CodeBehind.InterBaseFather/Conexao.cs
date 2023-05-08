e2ea33c6b723c0f32bc7603c4d6f5b81f79d8bcc
Rodolfo Fonseca <rodolfocomp@gmail.com>
2023-05-08 20:11:36 -0300

    Interbase SQL

---
+++ CodeBehind/CodeBehind.InterBaseFather/Conexao.cs
@@ -0,0 +1,18 @@
+﻿using InterBaseSql.Data.InterBaseClient;
+
+namespace CodeBehind.InterBaseFather
+{
+    public class Conexao
+    {
+        public static IBConnectionStringBuilder BuildConnectionStringBuilder()
+        {
+            var builder = new IBConnectionStringBuilder();
+            builder.UserID = "SYSDBA";
+            builder.Password = "masterkey";
+            builder.DataSource = "localhost";
+            builder.Database = "C:\\temp\\BANCO.IB";
+            builder.Port = 3050;
+            return builder;
+        }
+    }
+}
---
+++ CodeBehind/CodeBehind.InterBaseFather/Program.cs
@@ -0,0 +1,40 @@
+﻿using CodeBehind.InterBaseFather;
+using Dapper;
+using InterBaseSql.Data.InterBaseClient;
+
+//CONEXAO DIA ADO ---------------------------------------------------------------------
+var con = new IBConnection(Conexao.BuildConnectionStringBuilder().ToString());
+con.Open();
+
+var cmd = con.CreateCommand();
+cmd.CommandText = "SELECT * FROM TMP$POOLS";
+var rd = cmd.ExecuteReader();
+if (rd.HasRows)
+{
+    while (rd.Read())
+    {
+        Console.WriteLine(rd[1].ToString());
+    }
+}
+con.Close();
+//FIM-CONEXAO DIA ADO ---------------------------------------------------------------------
+
+
+
+
+//CONEXAO VIA DAPPER ----------------------------------------------------------------------
+using (var conDap = new IBConnection(Conexao.BuildConnectionStringBuilder().ToString()))
+{
+
+    var res = conDap.Query<string>("SELECT TMP$TYPE FROM TMP$POOLS");
+
+    var maxId = conDap.QueryFirst<int>("SELECT MAX(ID) AS ID FROM CLIENTE") + 1;
+
+    conDap.Execute($"INSERT INTO CLIENTE VALUES ({maxId},'LUCAS DA SILVA {maxId}')");
+
+    conDap.Execute("UPDATE CLIENTE SET NOME = 'MARIA DA SILVA GOMES' WHERE ID = 2");
+
+    conDap.Execute($"DELETE FROM CLIENTE WHERE ID = {maxId}");
+}
+
+//FIM-CONEXAO VIA DAPPER -----------------------------------------------------------------
